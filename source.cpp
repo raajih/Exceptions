0fc29aea56002c601fc6afad56e9b88c2195f463
Raajih Roland <raajihroland88@gmail.com>
2024-07-22 12:51:46 -0400

    Initial character function before testing

--- source.cpp
+++ source.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <cctype>//
 using namespace std;
 
 
-char character(char start, int offset);
+char character(char start, int offset);//Prototype for offset function
 
-class InvalidCharacterException{};
-class InvalidRangeException{};
+//Exceptions
+class invalidCharacterException{};
+class invalidRangeException{};
 
 int main()
 {
 
-
+	char test = 'a';
+	cout << character(test, 1);
 
 	return 0;
+}
+
+char character(char start, int offset)
+{
+	char result = start + offset;//The value we want to return
+
+	if (!isalpha(start))//If start isn't a valid character
+		throw invalidCharacterException();
+	else if (!isalpha(result))//If the offset reaches out of bounds 
+		throw invalidRangeException();
+	else
+		return result;
 }
