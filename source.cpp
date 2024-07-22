6f114ac0d0d7c4d38f9831d2ba6df78615d6c13e
Raajih Roland <raajihroland88@gmail.com>
2024-07-22 12:38:47 -0400

    Prototype and exceptions created

---
+++ source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+char character(char start, int offset);
+
+class InvalidCharacterException{};
+class InvalidRangeException{};
+
+int main()
+{
+
+
+
+	return 0;
+}
